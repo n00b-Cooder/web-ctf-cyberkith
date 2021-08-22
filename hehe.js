6f21e4ab3952b910bf91fa7ef59d46b5518157af
fake <84218755+n00b-Cooder@users.noreply.github.com>
2021-08-22 08:14:09 +0530

    Add files via upload

---
+++ hehe.js
@@ -0,0 +1,23 @@
+function openTab(tabName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    if(elmnt.style != null) {
+	elmnt.style.backgroundColor = color;
+    }
+}
+
+window.onload = function() {
+    openTab('tabintro', this, '#222');
+}
+
+/* So this ctf has 3 flags */
+/* Flag format : CYB3R-K1TH+CTF5{******} */
+/* Here is your second flag = CYB3R-K1TH+CTF5{Fl4g@2} */ 
